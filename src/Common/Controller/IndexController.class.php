9b0526e3d2f24511d93100270909f104a8354c1e
leonNJ <1119679388@qq.com>
2017-05-06 14:30:01 +0800

    01

---
+++ src/Common/Controller/IndexController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Marmot\Common\Controller;
+
+class IndexController
+{
+	public function index()
+    {
+        echo "This is index Controller!<br />";
+    }
+
+}
---
+++ src/Common/Model/Index.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Marmot\Common\Model;
+
+class Index
+{
+	public function index()
+    {
+        echo "This is index model!<br />";
+    }
+
+}
